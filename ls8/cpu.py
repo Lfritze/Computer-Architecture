b1ddd55c4472b446b25380de3b51926b7e2e4f17
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-08-07 21:33:04 -0400

    MVP - cleaned up whitespace

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,22 +7,24 @@ HLT = 0b00000001
 PRN = 0b01000111
 MUL = 0b10100010
 NOP = 0b00000000
+POP = 0b01000110
+RET = 0b00010001
+CALL = 0b01010000
+PUSH = 0b01000101
+SP = 0b00000111
+ADD = 0b10100000
 
 class CPU:
     """Main CPU class."""
-
     def __init__(self):
-        """Construct a new CPU."""
         self.ram = [0] * 256
         self.reg = [0] * 8
         self.pc = 0
         self.running = True
 
     def load(self):
-        """Load a program into memory."""
-
-        # address = 0
         filename = sys.argv[1]
+
         with open(filename) as f:
             for address, line in enumerate(f):
                 line = line.split('#')
@@ -33,24 +35,6 @@ class CPU:
                 self.ram_write(address, v)
 
 
-
-        # For now, we've just hardcoded a program:
-
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010, # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111, # PRN R0
-        #     0b00000000,
-        #     0b00000001, # HLT
-        # ]
-
-        # for instruction in program:
-        #     self.ram[address] = instruction
-        #     address += 1
-
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
@@ -58,7 +42,6 @@ class CPU:
             self.reg[reg_a] += self.reg[reg_b]
         elif op == "MUL":
             self.reg[reg_a] *= self.reg[reg_b]
-        #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -90,35 +73,73 @@ class CPU:
 
     def HLT(self):
         self.running = False
-
+    
     def PRN(self):
         reg_num = self.ram_read(self.pc + 1)
         print(self.reg[reg_num])
         self.pc += 2
-
+    
     def MUL(self):
         reg_num1 = self.ram_read(self.pc + 1)
         reg_num2 = self.ram_read(self.pc + 2)
         self.alu("MUL", reg_num1, reg_num2)
         self.pc += 3
 
+    def ADD(self):
+        reg_num1 = self.ram_read(self.pc + 1)
+        reg_num2 = self.ram_read(self.pc + 2)
+        self.alu("ADD", reg_num1, reg_num2)
+        self.pc += 3
+
     def NOP(self):
         self.pc += 1
 
+    def PUSH(self):
+        reg_num = self.ram[self.pc + 1]
+        value = self.reg[reg_num]
+        self.reg[SP] -= 1
+        top_of_stack_add = self.reg[SP]
+        self.ram[top_of_stack_add] = value
+        self.pc += 2
+
+    def POP(self):
+        top_of_stack_add = self.reg[SP]
+        value = self.ram[top_of_stack_add]
+        reg_num = self.ram[self.pc + 1]
+        self.reg[reg_num] = value
+        self.reg[SP] += 1
+        self.pc += 2
+
+    def CALL(self):
+        return_addr = self.pc + 2
+
+        self.reg[SP] -= 1
+        self.ram[self.reg[SP]] = return_addr
+
+        reg_num = self.ram[self.pc + 1]
+        subroutine_addr = self.reg[reg_num]
+
+        self.pc = subroutine_addr
+
+    def RET(self):
+        subroutine_addr = self.ram[self.reg[SP]]
+        self.reg[SP] += 1
+        self.pc = subroutine_addr
+        
     def call_fun(self, n):
         branch_table = {
-            # 0 : self.NOP,
-            # 1 : self.HLT,
-            # 71 : self.PRN,
-            # 130 : self.LDI,
-            # 162 : self.MUL
-
-            NOP: self.NOP,
+            NOP : self.NOP,
             HLT : self.HLT,
             PRN : self.PRN,
             LDI : self.LDI,
-            MUL : self.MUL
+            MUL : self.MUL,
+            ADD : self.ADD,
+            PUSH : self.PUSH,
+            POP : self.POP,
+            CALL : self.CALL,
+            RET : self.RET
         }
+
         f = branch_table[n]
         if branch_table.get(n) is not None:
             f()
@@ -127,10 +148,10 @@ class CPU:
             sys.exit(1)
 
     def run(self):
-        """Run the CPU."""
         while self.running:
             ir = self.ram_read(self.pc)
             self.call_fun(ir)
+            
 
     def ram_read(self, address):
         return self.ram[address]
